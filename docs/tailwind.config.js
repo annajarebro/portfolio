50a8b1b858b9108f2c5b2091b9dff43c8b794e43
Anna Jarebro <annajarebro@AnnaJ-M1Pro.local>
2023-11-14 20:55:59 +0100

    build tailwind for prod, remove cdn

---
+++ docs/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+	"./**/*.{html,js}",
+	"./node_modules/tw-elements/dist/js/**/*.js"
+],
+theme: {
+extend: {
+  animation: {
+    'infinite-scroll': 'infinite-scroll 30s linear infinite',
+  },
+
+  keyframes: {
+    'infinite-scroll': {
+      from: { transform: 'translateX(0)' },
+      to: { transform: 'translateX(-100%)' },
+    }
+  }
+},
+},
+  plugins: [require("tw-elements/dist/plugin.cjs")],
+  darkMode: "class"
+}
+
