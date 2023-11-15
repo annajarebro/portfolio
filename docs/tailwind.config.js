c71cb7c17c681b1da5d1568b9d436697beb69a10
Anna Jarebro <annajarebro@AnnaJ-M1Pro.local>
2023-11-15 09:49:31 +0100

    Hide carousel and fixed width on Stravito project

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -22,3 +22,13 @@ extend: {
   darkMode: "class"
 }
 
+// tailwind.config.js
+module.exports = {
+  theme: {
+    // ...
+  },
+  plugins: [
+    require('tailwind-scrollbar-hide')
+    // ...
+  ]
+}
